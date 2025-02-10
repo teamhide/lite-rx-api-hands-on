6c14470d1326888adba28a1af695a6ab4987fea3
Hide <padocon@naver.com>
2025-02-10 19:07:21 +0900

    Part 01 done

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -2,6 +2,8 @@ package io.pivotal.literx;
 
 import reactor.core.publisher.Flux;
 
+import java.util.Arrays;
+
 /**
  * Learn how to create Flux instances.
  *
@@ -14,28 +16,28 @@ public class Part01Flux {
 
 	// TODO Return an empty Flux
 	Flux<String> emptyFlux() {
-		return null;
+		return Flux.empty();
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
 	Flux<String> fooBarFluxFromValues() {
-		return null;
+		return Flux.just("foo", "bar");
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
 	Flux<String> fooBarFluxFromList() {
-		return null;
+		return Flux.fromIterable(Arrays.asList("foo", "bar"));
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux that emits an IllegalStateException
 	Flux<String> errorFlux() {
-		return null;
+		return Flux.error(new IllegalStateException());
 	}
 
 //========================================================================================
